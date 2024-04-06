47ede82b4724e7b00a3dfd28f279b5c3d8a184c8
wmxpy <wm@mengw.io>
2024-04-06 16:46:29 -0500

    Update ImbricateOriginCollection interface in collection module

--- src/collection/interface.ts
+++ src/collection/interface.ts
@@ -7,7 +7,7 @@
 import { PromiseOr } from "../definition/promise";
 import { ImbricatePageMetadata, ImbricatePageSearchSnippet, ImbricatePageSnapshot } from "../page/definition";
 import { IImbricatePage } from "../page/interface";
-import { IMBRICATE_COLLECTION_TRIGGER_TYPE, ImbricateCollectionEvent } from "./definition";
+import { IMBRICATE_COLLECTION_EVENT_TYPE, IMBRICATE_COLLECTION_TRIGGER_TYPE, ImbricateCollectionEvent } from "./definition";
 
 export interface IImbricateOriginCollection {
 
@@ -15,6 +15,10 @@ export interface IImbricateOriginCollection {
     readonly description?: string;
 
     readonly events: Array<ImbricateCollectionEvent<IMBRICATE_COLLECTION_TRIGGER_TYPE>>;
+
+    attachEvent(event: ImbricateCollectionEvent<IMBRICATE_COLLECTION_TRIGGER_TYPE>): PromiseOr<void>;
+    getEventsByEventType(eventType: IMBRICATE_COLLECTION_EVENT_TYPE): PromiseOr<Array<ImbricateCollectionEvent<IMBRICATE_COLLECTION_TRIGGER_TYPE>>>;
+    triggerEvent(eventType: IMBRICATE_COLLECTION_EVENT_TYPE, payload: any): PromiseOr<void>;
 
     createPage(title: string, initialContent?: string): PromiseOr<IImbricatePage>;
     putPage(pageMetadata: ImbricatePageMetadata, content: string): PromiseOr<IImbricatePage>;
