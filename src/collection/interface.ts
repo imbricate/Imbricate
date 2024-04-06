cc9ddc26c4f35fa1296505c775cf10ba7aa1b5b5
wmxpy <wm@mengw.io>
2024-04-06 12:18:15 -0500

    Add putPage method to IImbricateOriginCollection interface

--- src/collection/interface.ts
+++ src/collection/interface.ts
@@ -6,7 +6,7 @@
 
 import { PromiseOr } from "../definition/promise";
 import { ImbricatePageSnapshot } from "../page/definition";
-import { IImbricatePage } from "../page/interface";
+import { IImbricatePage, ImbricatePageMetadata } from "../page/interface";
 import { IMBRICATE_SEARCH_SNIPPET_TYPE, ImbricateSearchSnippet } from "../search/snippet";
 
 export type ImbricatePageSearchSnippet = ImbricateSearchSnippet<IMBRICATE_SEARCH_SNIPPET_TYPE.PAGE>;
@@ -17,6 +17,7 @@ export interface IImbricateOriginCollection {
     readonly description?: string;
 
     createPage(title: string, initialContent?: string): PromiseOr<IImbricatePage>;
+    putPage(pageMetadata: ImbricatePageMetadata, content: string): PromiseOr<IImbricatePage>;
     retitlePage(identifier: string, newTitle: string): PromiseOr<void>;
     deletePage(identifier: string, title: string): PromiseOr<void>;
     hasPage(title: string): PromiseOr<boolean>;
