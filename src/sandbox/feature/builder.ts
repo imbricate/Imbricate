6142a459b3f9fc785241818822913ec1f2285a87
wmxpy <wm@mengw.io>
2024-04-11 00:40:33 -0500

    Refactor SandboxFeatureBuilder to support different providers

--- src/sandbox/feature/builder.ts
+++ src/sandbox/feature/builder.ts
@@ -9,16 +9,43 @@ import { SandboxFeature } from "./feature";
 
 export class SandboxFeatureBuilder {
 
-    public static fromScratch(): SandboxFeatureBuilder {
+    public static providedByOrigin(): SandboxFeatureBuilder {
 
-        return new SandboxFeatureBuilder();
+        return new SandboxFeatureBuilder(
+            "origin",
+        );
     }
+
+    public static providedBySandbox(): SandboxFeatureBuilder {
+
+        return new SandboxFeatureBuilder(
+            "sandbox",
+        );
+    }
+
+    public static providedByInterface(): SandboxFeatureBuilder {
+
+        return new SandboxFeatureBuilder(
+            "interface",
+        );
+    }
+
+    public static providedBy(providedBy: string): SandboxFeatureBuilder {
+
+        return new SandboxFeatureBuilder(providedBy);
+    }
+
+    private readonly _provideBy: string;
 
     private _packageName: string;
     private _methodName: string;
     private _implementation: SandboxImplementation;
 
-    private constructor() {
+    private constructor(
+        provideBy: string,
+    ) {
+
+        this._provideBy = provideBy;
 
         this._packageName = "";
         this._methodName = "";
@@ -46,7 +73,7 @@ export class SandboxFeatureBuilder {
     public build(): SandboxFeature {
 
         return SandboxFeature.create(
-            this._packageName,
+            `${this._provideBy}:${this._packageName}`,
             this._methodName,
             this._implementation,
         );
