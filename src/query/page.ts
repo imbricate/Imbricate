57424aaee202e0c7bcff3feb4c2e43889396eb11
wmxpy <wm@mengw.io>
2024-04-14 20:28:17 -0500

    Update ImbricatePageQuery and ImbricateScriptQuery interfaces in query module

--- src/query/page.ts
+++ src/query/page.ts
@@ -4,7 +4,16 @@
  * @description Page
  */
 
+import { ImbricateQueryString, ImbricateQueryTime } from "./definition";
+
 export type ImbricatePageQuery = {
 
     readonly limit?: number;
+    readonly skip?: number;
+
+    readonly title?: ImbricateQueryString;
+    readonly attributes?: Record<string, ImbricateQueryString>;
+
+    readonly createdAt?: ImbricateQueryTime;
+    readonly updatedAt?: ImbricateQueryTime;
 };
--- src/query/script.ts
+++ src/query/script.ts
@@ -4,7 +4,16 @@
  * @description Script
  */
 
+import { ImbricateQueryString, ImbricateQueryTime } from "./definition";
+
 export type ImbricateScriptQuery = {
 
     readonly limit?: number;
+    readonly skip?: number;
+
+    readonly scriptName?: ImbricateQueryString;
+    readonly attributes?: Record<string, ImbricateQueryString>;
+
+    readonly createdAt?: ImbricateQueryTime;
+    readonly updatedAt?: ImbricateQueryTime;
 };
