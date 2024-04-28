baa35bb5c84ad6c4551252cc89e766db7d0c5dad
wmxpy <wm@mengw.io>
2024-04-27 22:04:14 -0500

    Update version to 1.14.3 in package.json

--- src/page/definition.ts
+++ src/page/definition.ts
@@ -17,7 +17,7 @@ export type ImbricatePageSearchSnippet =
 export type ImbricatePageSnapshot = {
 
     readonly title: string;
-    readonly paths: string[];
+    readonly directories: string[];
 
     readonly identifier: string;
 };
