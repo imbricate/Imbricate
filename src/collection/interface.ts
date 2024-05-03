000fca0631d3678200b7d6a463c21269c2e5e0d5
wmxpy <wm@mengw.io>
2024-05-03 11:32:55 -0500

    feat: Add includeInSearch property to IImbricateOriginCollection interface

--- src/collection/interface.ts
+++ src/collection/interface.ts
@@ -14,6 +14,8 @@ export interface IImbricateOriginCollection {
     readonly collectionName: string;
     readonly description?: string;
 
+    readonly includeInSearch: boolean;
+
     createPage(directories: string[], title: string, initialContent: string): PromiseOr<IImbricatePage>;
     putPage(pageMetadata: ImbricatePageMetadata, content: string): PromiseOr<IImbricatePage>;
     retitlePage(identifier: string, newTitle: string): PromiseOr<void>;
