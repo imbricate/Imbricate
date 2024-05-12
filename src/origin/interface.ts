4c927e2bb1b416a15cdafd1de21f2af86e96c8ff
wmxpy <wm@mengw.io>
2024-05-11 23:00:48 -0500

    refactor: Update IImbricateOrigin interface to include getFunctionManager and getBinaryStorage methods

--- src/origin/interface.ts
+++ src/origin/interface.ts
@@ -15,15 +15,41 @@ import { ImbricateOriginCapability, ImbricateOriginMetadata } from "./definition
 
 export interface IImbricateOrigin {
 
+    /**
+     * Origin type
+     */
     readonly originType: string;
+    /**
+     * Unique identifier of the origin
+     */
     readonly uniqueIdentifier: string;
 
+    /**
+     * Metadata of the origin
+     */
     readonly metadata: ImbricateOriginMetadata;
+    /**
+     * Payloads of the origin
+     */
     readonly payloads: Record<string, any>;
 
+    /**
+     * Capabilities of the origin
+     */
     readonly capabilities: ImbricateOriginCapability;
 
+    /**
+     * Get function manager
+     * 
+     * @returns Function manager
+     */
     getFunctionManger(): IImbricateFunctionManager;
+
+    /**
+     * Get binary storage
+     * 
+     * @returns Binary storage
+     */
     getBinaryStorage(): IImbricateBinaryStorage;
 
     /**
@@ -31,8 +57,10 @@ export interface IImbricateOrigin {
      * 
      * @param collectionName Collection name
      * @param description Collection description
+     * 
+     * @returns Created collection
      */
-    createCollection(collectionName: string, description?: string): PromiseOr<void>;
+    createCollection(collectionName: string, description?: string): PromiseOr<IImbricateOriginCollection>;
     renameCollection(collectionUniqueIdentifier: string, newCollectionName: string): PromiseOr<void>;
     deleteCollection(collectionUniqueIdentifier: string): PromiseOr<void>;
     hasCollection(collectionName: string): PromiseOr<boolean>;
@@ -40,6 +68,15 @@ export interface IImbricateOrigin {
     getCollection(collectionUniqueIdentifier: string): PromiseOr<IImbricateOriginCollection | null>;
     listCollections(): PromiseOr<IImbricateOriginCollection[]>;
 
+    /**
+     * Create a script
+     * 
+     * @param scriptName Script name
+     * @param initialScript Initial script content
+     * @param description Script description
+     * 
+     * @returns Created script
+     */
     createScript(scriptName: string, initialScript: string, description?: string): PromiseOr<IImbricateScript>;
     putScript(scriptMetadata: ImbricateScriptMetadata, script: string): PromiseOr<IImbricateScript>;
     renameScript(identifier: string, newScriptName: string): PromiseOr<void>;
