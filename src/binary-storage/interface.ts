a42f7b176da49c51fec111e6647b4f60bcd25107
wmxpy <wm@mengw.io>
2024-05-11 22:21:44 -0500

    refactor: Update IImbricateBinaryStorage interface to include putBinaryBase64 and validateBinaryBase64 methods

--- src/binary-storage/interface.ts
+++ src/binary-storage/interface.ts
@@ -8,5 +8,18 @@ import { PromiseOr } from "../definition/promise";
 
 export interface IImbricateBinaryStorage {
 
+    /**
+     * Store a binary file
+     * 
+     * @param binary Base64 encoded binary
+     * @param fileName File name
+     */
     putBinaryBase64(binary: string, fileName: string): PromiseOr<string>;
+
+    /**
+     * Validate if the binary is a valid base64 to be stored
+     * 
+     * @param binary Base64 encoded binary
+     */
+    validateBinaryBase64(binary: string): PromiseOr<boolean>;
 }
