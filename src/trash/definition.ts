651bb4b19780f152a83ca74aeab4a254f8ca7d38
wmxpy <wm@mengw.io>
2024-05-13 17:36:48 -0500

    feat: Add tossPage and retrievePage methods to IImbricateTrashStash interface

---
+++ src/trash/definition.ts
@@ -0,0 +1,13 @@
+/**
+ * @author WMXPY
+ * @namespace Trash
+ * @description Definition
+ */
+
+export type ImbricateVacantPage = {
+
+    readonly collectionUniqueIdentifier: string;
+    readonly pageIdentifier: string;
+    readonly contentDigest: string;
+    readonly content: string;
+};
--- src/trash/interface.ts
+++ src/trash/interface.ts
@@ -4,7 +4,11 @@
  * @description Interface
  */
 
+import { PromiseOr } from "../definition/promise";
+import { ImbricateVacantPage } from "./definition";
+
 export interface IImbricateTrashStash {
 
-
+    tossPage(page: ImbricateVacantPage): PromiseOr<void>;
+    retrievePage(collectionUniqueIdentifier: string, pageIdentifier: string): PromiseOr<ImbricateVacantPage | null>;
 }
