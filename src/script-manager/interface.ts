6f860d4de9be3760d0ce40f22e35267b90e76ca0
wmxpy <wm@mengw.io>
2024-06-06 14:39:25 -0500

    feat: Refactor script manager interface and remove unused imports

--- src/script-manager/interface.ts
+++ src/script-manager/interface.ts
@@ -4,69 +4,18 @@
  * @description Interface
  */
 
-import { IImbricateBinaryStorage } from "../binary-storage/interface";
-import type { IImbricateCollection } from "../collection/interface";
 import { PromiseOr } from "../definition/promise";
-import { IImbricateFunctionManager } from "../function/interface";
 import { ImbricateScriptQuery, ImbricateScriptQueryConfig, ImbricateSearchScriptConfig } from "../query/script";
 import { ImbricateScriptMetadata, ImbricateScriptSearchResult, ImbricateScriptSnapshot } from "../script/definition";
 import { IImbricateScript } from "../script/interface";
-import { ImbricateOriginCapability, ImbricateOriginMetadata } from "./definition";
+import { ImbricateScriptManagerCapability } from "./definition";
 
 export interface IImbricateScriptManager {
 
     /**
-     * Origin type
-     */
-    readonly originType: string;
-    /**
-     * Unique identifier of the origin
-     */
-    readonly uniqueIdentifier: string;
-
-    /**
-     * Metadata of the origin
-     */
-    readonly metadata: ImbricateOriginMetadata;
-    /**
-     * Payloads of the origin
-     */
-    readonly payloads: Record<string, any>;
-
-    /**
      * Capabilities of the origin
      */
-    readonly capabilities: ImbricateOriginCapability;
-
-    /**
-     * Get function manager
-     * 
-     * @returns Function manager
-     */
-    getFunctionManager(): IImbricateFunctionManager;
-
-    /**
-     * Get binary storage
-     * 
-     * @returns Binary storage
-     */
-    getBinaryStorage(): IImbricateBinaryStorage;
-
-    /**
-     * Create a collection
-     * 
-     * @param collectionName Collection name
-     * @param description Collection description
-     * 
-     * @returns Created collection
-     */
-    createCollection(collectionName: string, description?: string): PromiseOr<IImbricateCollection>;
-    renameCollection(collectionUniqueIdentifier: string, newCollectionName: string): PromiseOr<void>;
-    deleteCollection(collectionUniqueIdentifier: string): PromiseOr<void>;
-    hasCollection(collectionName: string): PromiseOr<boolean>;
-    findCollection(collectionName: string): PromiseOr<IImbricateCollection | null>;
-    getCollection(collectionUniqueIdentifier: string): PromiseOr<IImbricateCollection | null>;
-    listCollections(): PromiseOr<IImbricateCollection[]>;
+    readonly capabilities: ImbricateScriptManagerCapability;
 
     /**
      * Create a script
@@ -87,10 +36,4 @@ export interface IImbricateScriptManager {
 
     searchScripts(keyword: string, config: ImbricateSearchScriptConfig): PromiseOr<ImbricateScriptSearchResult[]>;
     queryScripts(query: ImbricateScriptQuery, config: ImbricateScriptQueryConfig): PromiseOr<IImbricateScript[]>;
-
-    /**
-     * Dispose the origin, optional
-     * This method will be called when the origin is no longer needed
-     */
-    dispose?(): PromiseOr<void>;
 }
