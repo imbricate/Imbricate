b7f249fd01dec6d8da349585a06f5ccafee63f79
wmxpy <wm@mengw.io>
2024-07-07 15:12:44 -0500

    refactor: Update ImbricateScriptMetadata and ImbricatePageMetadata definitions, and remove unused code

--- src/page/interface.ts
+++ src/page/interface.ts
@@ -31,9 +31,34 @@ export interface IImbricatePage {
     readAttributes(): PromiseOr<ImbricatePageAttributes>;
     writeAttribute(key: string, value: string): PromiseOr<void>;
 
+    /**
+     * Refresh Updated At and Digest, and add a history record
+     * Recommended to use this method when updating the page
+     * 
+     * @param updatedAt Updated At
+     * @param digest Content Digest
+     */
     refreshUpdateMetadata(updatedAt: Date, digest: string): PromiseOr<void>;
 
+    /**
+     * Refresh Updated At
+     * Prefer `refreshUpdateMetadata` in most cases
+     * 
+     * @param updatedAt Updated At
+     */
     refreshUpdatedAt(updatedAt: Date): PromiseOr<void>;
+    /**
+     * Refresh Digest
+     * Prefer `refreshUpdateMetadata` in most cases
+     * 
+     * @param digest Content Digest
+     */
     refreshDigest(digest: string): PromiseOr<void>;
+    /**
+     * Add a history record
+     * Prefer `refreshUpdateMetadata` in most cases
+     * 
+     * @param record History Record
+     */
     addHistoryRecord(record: ImbricatePageHistoryRecord): PromiseOr<void>;
 }
--- src/script/interface.ts
+++ src/script/interface.ts
@@ -30,9 +30,34 @@ export interface IImbricateScript {
     readAttributes(): PromiseOr<ImbricateScriptAttributes>;
     writeAttribute(key: string, value: string): PromiseOr<void>;
 
+    /**
+     * Refresh Updated At and Digest, and add a history record
+     * Recommended to use this method when updating the script
+     * 
+     * @param updatedAt Updated At
+     * @param digest Content Digest
+     */
     refreshUpdateMetadata(updatedAt: Date, digest: string): PromiseOr<void>;
 
+    /**
+     * Refresh Updated At
+     * Prefer `refreshUpdateMetadata` in most cases
+     * 
+     * @param updatedAt Updated At
+     */
     refreshUpdatedAt(updatedAt: Date): PromiseOr<void>;
+    /**
+     * Refresh Digest
+     * Prefer `refreshUpdateMetadata` in most cases
+     * 
+     * @param digest Content Digest
+     */
     refreshDigest(digest: string): PromiseOr<void>;
+    /**
+     * Add a history record
+     * Prefer `refreshUpdateMetadata` in most cases
+     * 
+     * @param record History Record
+     */
     addHistoryRecord(record: ImbricateScriptHistoryRecord): PromiseOr<void>;
 }
