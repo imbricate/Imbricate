3b191d9a2f46af5145b530854bcb587c3dc8fdd7
wmxpy <wm@mengw.io>
2024-07-13 21:06:06 -0500

    feat: Update ImbricateAuthor definition with optional avatar and displayName properties

--- src/author/definition.ts
+++ src/author/definition.ts
@@ -4,10 +4,14 @@
  * @description Definition
  */
 
+export type ImbricateAuthorAttributes = Record<string, string>;
+
 export type ImbricateAuthor = {
 
-    readonly avatar: string;
-    readonly displayName: string;
+    readonly avatar?: string;
+    readonly displayName?: string;
 
     readonly identifier: string;
+
+    readonly attributes: ImbricateAuthorAttributes;
 };
