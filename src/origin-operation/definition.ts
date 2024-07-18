ce43024a2406e5d6f4fa76f1285e2e83d35a1c3f
wmxpy <wm@mengw.io>
2024-07-18 17:31:19 -0500

    feat: Add ImbricateOriginOperationManager interface

--- src/origin-operation/definition.ts
+++ src/origin-operation/definition.ts
@@ -4,7 +4,23 @@
  * @description Definition
  */
 
+import { ImbricateCapability } from "../capability/definition";
 import { PromiseOr } from "../definition/promise";
+
+export type ImbricateOriginOperationManagerCapability =
+    Record<IMBRICATE_ORIGIN_OPERATION_MANAGER_CAPABILITY_KEY, ImbricateCapability>;
+
+export enum IMBRICATE_ORIGIN_OPERATION_MANAGER_CAPABILITY_KEY {
+
+    LIST_OPERATIONS = "imbricate.origin-operation.list",
+    EXECUTE_OPERATION = "imbricate.origin-operation.execute",
+}
+
+export const ImbricateOriginOperationManagerCapabilityList: IMBRICATE_ORIGIN_OPERATION_MANAGER_CAPABILITY_KEY[] = [
+
+    IMBRICATE_ORIGIN_OPERATION_MANAGER_CAPABILITY_KEY.LIST_OPERATIONS,
+    IMBRICATE_ORIGIN_OPERATION_MANAGER_CAPABILITY_KEY.EXECUTE_OPERATION,
+];
 
 export enum IMBRICATE_ORIGIN_OPERATION_SCOPE {
 
@@ -14,6 +30,11 @@ export enum IMBRICATE_ORIGIN_OPERATION_SCOPE {
 }
 
 export type ImbricateOriginOperation<T extends IMBRICATE_ORIGIN_OPERATION_SCOPE> = {
+
+    readonly operationIdentifier: string;
+
+    readonly operationDisplayName?: string;
+    readonly operationDescription?: string;
 
     readonly scope: T;
 
