02d8f5985512694bafe14dfb9067ff847a516833
wmxpy <wm@mengw.io>
2024-07-18 17:27:32 -0500

    feat: Add PromiseOr import to OriginOperation definition file

--- src/origin-operation/definition.ts
+++ src/origin-operation/definition.ts
@@ -4,6 +4,8 @@
  * @description Definition
  */
 
+import { PromiseOr } from "../definition/promise";
+
 export enum IMBRICATE_ORIGIN_OPERATION_SCOPE {
 
     ORIGIN = "imbricate.origin",
@@ -11,7 +13,17 @@ export enum IMBRICATE_ORIGIN_OPERATION_SCOPE {
     PAGE = "imbricate.page",
 }
 
-export type ImbricateOriginOperation = {
+export type ImbricateOriginOperation<T extends IMBRICATE_ORIGIN_OPERATION_SCOPE> = {
 
-    readonly scope: IMBRICATE_ORIGIN_OPERATION_SCOPE;
+    readonly scope: T;
+
+    readonly operation: (
+        parameter: ImbricateOriginOperationParameter<T>,
+    ) => PromiseOr<void>;
 };
+
+export type ImbricateOriginOperationParameter<T extends IMBRICATE_ORIGIN_OPERATION_SCOPE> =
+    T extends IMBRICATE_ORIGIN_OPERATION_SCOPE.ORIGIN ? any :
+    T extends IMBRICATE_ORIGIN_OPERATION_SCOPE.COLLECTION ? any :
+    T extends IMBRICATE_ORIGIN_OPERATION_SCOPE.PAGE ? any :
+    any;
