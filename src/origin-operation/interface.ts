8c0535f07c020a68cae1778e533d3b24c0c8e39e
wmxpy <wm@mengw.io>
2024-07-18 20:11:44 -0500

    feat: Update ImbricateOriginOperationManager interface

--- src/origin-operation/interface.ts
+++ src/origin-operation/interface.ts
@@ -4,52 +4,12 @@
  * @description Interface
  */
 
+import { ImbricateOriginOperationManagerCapability } from "./definition";
+
 export interface IImbricateOriginOperationManager {
 
     /**
      * Capabilities of the executable manager
      */
-    readonly capabilities: ImbricateExecutableManagerCapability;
-
-    /**
-     * Find supported variants
-     * 
-     * @param variant the partial variant to find, if not provided, find all
-     * 
-     * @returns supported variants
-     */
-    findSupportedVariants(
-        variant?: Partial<ImbricateScriptVariant>,
-    ): PromiseOr<ImbricateScriptVariant[]>;
-
-    /**
-     * Execute a script
-     * 
-     * @param script 
-     * @param parameters 
-     * @param environment 
-     * 
-     * @returns result of the execution
-     */
-    executeScript(
-        script: IImbricateScript,
-        parameters: ImbricateExecuteParameters,
-        environment: ImbricateExecuteEnvironment,
-    ): PromiseOr<ImbricateExecuteResult>;
-
-    /**
-     * Execute a snippet
-     * 
-     * @param snippet 
-     * @param parameters 
-     * @param environment 
-     * 
-     * @returns result of the execution
-     */
-    executeSnippet(
-        snippet: string,
-        variant: ImbricateScriptVariant,
-        parameters: ImbricateExecuteParameters,
-        environment: ImbricateExecuteEnvironment,
-    ): PromiseOr<ImbricateExecuteResult>;
+    readonly capabilities: ImbricateOriginOperationManagerCapability;
 }
