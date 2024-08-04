ffabb0bdd145e51eb2dcd3e775fe36a48de8f2cb
wmxpy <wm@mengw.io>
2024-08-03 19:56:21 -0500

    feat: Add page-variant/extension module
    This commit adds the page-variant/extension module to the project. This module provides functions for handling file extensions based on the page variant language. It includes a function to get the file extension for a given language and an option to include a dot before the extension. This module will be useful for managing different file types in the project.

---
+++ src/page-variant/extension.ts
@@ -0,0 +1,34 @@
+/**
+ * @author WMXPY
+ * @namespace PageVariant
+ * @description Extension
+ */
+
+import { IMBRICATE_PAGE_VARIANT_LANGUAGE } from "./definition";
+
+const fixExtension = (
+    extension: string,
+    withDot: boolean,
+): string => {
+
+    if (withDot) {
+        return `.${extension}`;
+    }
+
+    return extension;
+};
+
+export const getImbricateScriptLanguageExtension = (
+    language: IMBRICATE_PAGE_VARIANT_LANGUAGE,
+    withDot: boolean = true,
+): string => {
+
+    switch (language) {
+
+        case IMBRICATE_PAGE_VARIANT_LANGUAGE.TEXT: return fixExtension("txt", withDot);
+        case IMBRICATE_PAGE_VARIANT_LANGUAGE.MARKDOWN: return fixExtension("md", withDot);
+        case IMBRICATE_PAGE_VARIANT_LANGUAGE.LATEX: return fixExtension("tex", withDot);
+    }
+
+    return fixExtension("unknown", withDot);
+};
