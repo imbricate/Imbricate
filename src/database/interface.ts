844a1364ddeb9c670ff47f763cd5eb0bebd7514c
wmxpy <wm@mengw.io>
2024-10-22 13:41:10 -0500

    Refactor database interface

---
+++ src/database/interface.ts
@@ -0,0 +1,13 @@
+/**
+ * @author WMXPY
+ * @namespace Origin
+ * @description Database
+ */
+
+export interface IImbricateDatabase {
+
+    /**
+     * Unique identifier of the database
+     */
+    readonly uniqueIdentifier: string;
+}
