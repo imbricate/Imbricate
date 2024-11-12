f4536940b8a15ee26b21ffc4a80353c3ecb4308e
wmxpy <wm@mengw.io>
2024-11-12 14:52:34 -0600

    refactor origin loader to separate loading logic and return null for non-existent origins

--- src/loader/origin-loader.ts
+++ src/loader/origin-loader.ts
@@ -5,7 +5,49 @@
  */
 
 import { IImbricateOrigin } from "../origin/interface";
-import { IMBRICATE_ORIGIN_LOAD_TYPE, ImbricateOriginPersistance } from "./persistance";
+import { IMBRICATE_ORIGIN_LOAD_TYPE, ImbricateOriginPersistance, ImbricateOriginPersistanceOrigin } from "./persistance";
+
+export const loadImbricateOriginFromPersistanceOrigin = async (
+    origin: ImbricateOriginPersistanceOrigin,
+): Promise<IImbricateOrigin | null> => {
+
+    switch (origin.originLoadType) {
+
+        case IMBRICATE_ORIGIN_LOAD_TYPE.NPM_PACKAGE: {
+
+            const originPackage = await import(origin.originLoadValue);
+
+            if (typeof originPackage.default === "function") {
+
+                const initialized = originPackage.default.call(
+                    null,
+                    origin.originPayloads,
+                );
+
+                return initialized;
+            }
+            break;
+        }
+
+        case IMBRICATE_ORIGIN_LOAD_TYPE.FILE_SYSTEM: {
+
+            const originPackage = await import(origin.originLoadValue);
+
+            if (typeof originPackage.default === "function") {
+
+                const initialized = originPackage.default.call(
+                    null,
+                    origin.originPayloads,
+                );
+
+                return initialized;
+            }
+            break;
+        }
+    }
+
+    return null;
+};
 
 export const loadImbricateOriginsFromPersistance = async (
     persistance: ImbricateOriginPersistance,
@@ -15,39 +57,10 @@ export const loadImbricateOriginsFromPersistance = async (
 
     for (const origin of persistance.origins) {
 
-        switch (origin.originLoadType) {
+        const originInstance: IImbricateOrigin | null = await loadImbricateOriginFromPersistanceOrigin(origin);
 
-            case IMBRICATE_ORIGIN_LOAD_TYPE.NPM_PACKAGE: {
-
-                const originPackage = await import(origin.originLoadValue);
-
-                if (typeof originPackage.default === "function") {
-
-                    const initialized = originPackage.default.call(
-                        null,
-                        origin.originPayloads,
-                    );
-
-                    origins.push(initialized);
-                }
-                break;
-            }
-
-            case IMBRICATE_ORIGIN_LOAD_TYPE.FILE_SYSTEM: {
-
-                const originPackage = await import(origin.originLoadValue);
-
-                if (typeof originPackage.default === "function") {
-
-                    const initialized = originPackage.default.call(
-                        null,
-                        origin.originPayloads,
-                    );
-
-                    origins.push(initialized);
-                }
-                break;
-            }
+        if (originInstance) {
+            origins.push(originInstance);
         }
     }
 
