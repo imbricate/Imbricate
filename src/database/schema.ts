4b9721fc9a8e3521b4a07bc0937ea93902cb7bac
wmxpy <wm@mengw.io>
2024-11-12 16:08:00 -0600

    add validation functions for Imbricate database schema and properties

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -26,3 +26,40 @@ export type ImbricateDatabaseSchemaForCreation = {
 
     readonly properties: ImbricateDatabaseSchemaPropertyForCreation[];
 };
+
+export const validateImbricateSchemaProperty = (property: ImbricateDatabaseSchemaProperty): boolean => {
+
+    if (typeof property.propertyIdentifier !== "string") {
+        return false;
+    }
+    if (typeof property.propertyName !== "string") {
+        return false;
+    }
+    if (!Object.values(IMBRICATE_PROPERTY_TYPE).includes(property.propertyType)) {
+        return false;
+    }
+
+    return true;
+};
+
+export const validateImbricateSchema = (schema: ImbricateDatabaseSchema): boolean => {
+
+    if (!Array.isArray(schema.properties)) {
+        return false;
+    }
+
+    const propertyNames: Set<string> = new Set();
+    for (const property of schema.properties) {
+
+        if (!validateImbricateSchemaProperty(property)) {
+            return false;
+        }
+
+        if (propertyNames.has(property.propertyName)) {
+            return false;
+        }
+        propertyNames.add(property.propertyName);
+    }
+
+    return true;
+};
