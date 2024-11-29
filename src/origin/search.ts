330265c1ca154c4e42670ca0faa9feecc9077d30
wmxpy <wm@mengw.io>
2024-11-29 15:21:56 -0600

    feat: add optional source and secondary context properties to ImbricateSearchItem type

--- src/origin/search.ts
+++ src/origin/search.ts
@@ -38,7 +38,22 @@ export type ImbricateSearchItem = {
     readonly target: ImbricateSearchTarget<IMBRICATE_SEARCH_TARGET_TYPE>;
 
     readonly primary: string;
+
+    /**
+     * @description Source are optional, use for source context
+     */
+    readonly sourceDatabaseName?: string;
+    readonly sourceDocumentPrimaryKey?: string;
+
+    /**
+     * @description Previous are optional, use for previous context on the secondary object
+     */
+    readonly secondaryPrevious?: string[];
     readonly secondary: string;
+    /**
+     * @description Next are optional, use for next context on the secondary object
+     */
+    readonly secondaryNext?: string[];
 };
 
 export type ImbricateSearchResult = {
