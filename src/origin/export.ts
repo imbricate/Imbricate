a3981cbba03ec1ae47782c3ac279baa942fb2245
wmxpy <wm@mengw.io>
2025-02-04 13:53:11 -0600

    fix export

--- src/origin/export.ts
+++ src/origin/export.ts
@@ -9,6 +9,7 @@ export * from "./base-class/exclude-static";
 export * from "./base-class/full-feature";
 export * from "./base-class/full-feature-with-action";
 export * from "./definition";
+export * from "./feature";
 export * from "./interface";
 export * from "./outcome";
 export * from "./search";
