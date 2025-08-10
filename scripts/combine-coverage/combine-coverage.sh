7bbfc15659d85a47e1bc95ea2feed764fe6a3ab7
wmxpy <wm@mengw.io>
2025-08-10 17:50:40 -0500

    feat: add combine coverage script and TypeScript configuration

---
+++ scripts/combine-coverage/combine-coverage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ts-node --project typescript/tsconfig.script.json scripts/combine-coverage/combine-coverage.ts
---
+++ scripts/combine-coverage/combine-coverage.ts
@@ -0,0 +1,14 @@
+/**
+ * @author WMXPY
+ * @description Combine Coverage
+ */
+
+import { join } from "node:path";
+
+(async () => {
+
+    const rootDirPath: string = join(__dirname, "../../");
+
+    // Packages Path
+    const packagesPath: string = join(rootDirPath, "packages");
+})();
