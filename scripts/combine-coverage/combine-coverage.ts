e9fa782bb4b3fb7be02506f2a6030480fdfee3da
wmxpy <wm@mengw.io>
2025-08-10 18:27:34 -0500

    Scans packages for coverage directories

--- scripts/combine-coverage/combine-coverage.ts
+++ scripts/combine-coverage/combine-coverage.ts
@@ -3,6 +3,8 @@
  * @description Combine Coverage
  */
 
+import type { Stats } from "node:fs";
+import { readdir, access } from "node:fs/promises";
 import { join } from "node:path";
 
 (async () => {
@@ -11,4 +13,14 @@ import { join } from "node:path";
 
     // Packages Path
     const packagesPath: string = join(rootDirPath, "packages");
+    const packagesFolders: string[] = await readdir(packagesPath);
+
+    for (const folder of packagesFolders) {
+        const packagePath: string = join(packagesPath, folder);
+        const coveragePath: string = join(packagePath, "coverage");
+
+        await access(coveragePath);
+
+        console.log(`Found coverage for ${folder}`);
+    }
 })();
