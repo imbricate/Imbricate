e1b114ccec804b7cafa561cca81d0ab601bdb867
wmxpy <wm@mengw.io>
2026-01-21 08:46:30 -0600

    feat: initialize new `@imbricate/stack-up` package with build, lint, and test configurations.

---
+++ packages/stack-up/jest.config.ts
@@ -0,0 +1,20 @@
+/**
+ * @namespace Root
+ * @description Jest Configuration
+ */
+
+import type { Config } from "jest";
+
+export default async (): Promise<Config> => {
+
+    return {
+        verbose: true,
+        collectCoverageFrom: [
+            "src/**/*.ts",
+        ],
+        coverageReporters: [
+            "json",
+            "text-summary",
+        ],
+    };
+};
