5f805659ddaf47028fbccb5b99a31eb30e8ea281
wmxpy <wm@mengw.io>
2026-02-07 11:34:42 -0600

    feat(core): Introduce built-in origin load type and enums

--- packages/core/src/loader/persistence.ts
+++ packages/core/src/loader/persistence.ts
@@ -8,13 +8,21 @@ import { OriginPayload } from "../origin/definition";
 /**
  * Imbricate Origin Load Type
  * 
+ * BUILT_IN - load origin from built-in, as origin name
  * NPM_PACKAGE - load origin from npm package, as package name
  * FILE_SYSTEM - load origin from file system, as file path
  */
 export enum IMBRICATE_ORIGIN_LOAD_TYPE {
 
+    BUILT_IN = "BUILT_IN",
     NPM_PACKAGE = "NPM_PACKAGE",
     FILE_SYSTEM = "FILE_SYSTEM",
+}
+
+export enum IMBRICATE_ORIGIN_BUILT_IN {
+
+    ORIGIN_FILE_SYSTEM = "origin-file-system",
+    ORIGIN_QUICK_ACTION = "origin-quick-action",
 }
 
 export type ImbricateOriginPersistenceOrigin = {
