fe3edd1dcc802d5bfd6fc976062f244bfd485a2e
wmxpy <wm@mengw.io>
2026-02-07 22:29:09 -0600

    feat: Implement `ImbricateQuickActionDatabase` with a constructor, essential properties, and new document query methods, extending `ImbricateDatabaseEssentialReadOnlyBase`.

--- packages-origin/quick-action/src/database/database.ts
+++ packages-origin/quick-action/src/database/database.ts
@@ -4,15 +4,60 @@
  * @description Database
  */
 
-import { IImbricateDatabase, ImbricateDatabaseGetDatabaseOutcome, ImbricateDatabaseQueryDatabasesOutcome, ImbricateDatabaseReadonlyBase, ImbricateDocumentQuery } from "@imbricate/core";
+import { IImbricateDatabase, ImbricateDatabaseCountDocumentsOutcome, ImbricateDatabaseEssentialReadOnlyBase, ImbricateDatabaseGetDatabaseOutcome, ImbricateDatabaseGetDocumentOutcome, ImbricateDatabaseQueryDatabasesOutcome, ImbricateDatabaseQueryDocumentsOutcome, ImbricateDatabaseReadonlyBase, ImbricateDatabaseSchema, ImbricateDocumentQuery } from "@imbricate/core";
 
-export class ImbricateQuickActionDatabase extends ImbricateDatabaseReadonlyBase implements IImbricateDatabase {
+export class ImbricateQuickActionDatabase extends ImbricateDatabaseEssentialReadOnlyBase implements IImbricateDatabase {
 
-    public async getDatabase(databaseUniqueIdentifier: string): Promise<ImbricateDatabaseGetDatabaseOutcome> {
+    public static create(
+        basePath: string,
+        uniqueIdentifier: string,
+        databaseName: string,
+        schema: ImbricateDatabaseSchema,
+    ): ImbricateQuickActionDatabase {
+
+        return new ImbricateQuickActionDatabase(
+            basePath,
+            uniqueIdentifier,
+            databaseName,
+            schema,
+        );
+    }
+
+    private readonly _basePath: string;
+
+    public readonly uniqueIdentifier: string;
+    public readonly databaseName: string;
+    public schema: ImbricateDatabaseSchema;
+
+    private constructor(
+        basePath: string,
+        uniqueIdentifier: string,
+        databaseName: string,
+        schema: ImbricateDatabaseSchema,
+    ) {
+        super();
+
+        this._basePath = basePath;
+
+        this.uniqueIdentifier = uniqueIdentifier;
+        this.databaseName = databaseName;
+        this.schema = schema;
+    }
+
+    public get databaseVersion(): string {
+
+        return "Default";
+    }
+
+    public getDocument(uniqueIdentifier: string): PromiseLike<ImbricateDatabaseGetDocumentOutcome> {
         throw new Error("Method not implemented.");
     }
 
-    public async queryDatabases(query: ImbricateDocumentQuery): Promise<ImbricateDatabaseQueryDatabasesOutcome> {
+    public queryDocuments(query: ImbricateDocumentQuery): PromiseLike<ImbricateDatabaseQueryDocumentsOutcome> {
+        throw new Error("Method not implemented.");
+    }
+
+    public countDocuments(query: ImbricateDocumentQuery): PromiseLike<ImbricateDatabaseCountDocumentsOutcome> {
         throw new Error("Method not implemented.");
     }
 }
